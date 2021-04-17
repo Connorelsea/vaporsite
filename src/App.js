e42cad58ac2c77df68418bc4b69cfa3581ea2766
Connor Elsea <connorelsea@gmail.com>
2021-04-17 00:31:44 -0500

    remove test window

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <ResumeWindow></ResumeWindow>
-      <SkillsWindow></SkillsWindow>
+      {/* <SkillsWindow></SkillsWindow> */}
     </div>
   );
 }
